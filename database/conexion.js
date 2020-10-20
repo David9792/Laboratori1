246c0249392f14369a19ffd2170fffc1f7a13fc4
david9792 <davit.el.unicodj@gmail.com>
2020-10-19 21:55:23 -0400

    Version1

--- database/conexion.js
+++ database/conexion.js
@@ -1,3 +1,3 @@
 const mongoose =require('mongoose');
-mongoose.connect("mongodb://192.168.1.101:27017/PRACTICA1");
+mongoose.connect("mongodb://192.168.1.101:27017/PRACTICA");
 module.exports = mongoose;
